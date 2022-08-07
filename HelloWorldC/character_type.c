e84f4d54b003fa1d07425d5cd8772c2ccffe689b
mark <978188440@qq.com>
2022-08-07 22:08:36 +0800

    提交

--- HelloWorldC/character_type.c
+++ HelloWorldC/character_type.c
@@ -3,6 +3,7 @@
 //
 #include "head/io_utils.h"
 #include <ctype.h>
+#include <time.h>
 
 /**
  * 判断是否未数字
@@ -16,6 +17,14 @@ int IsDigit(char c) {
 }
 
 int main() {
-    PRINTLN_INT(isdigit('9'));
+    time_t t1 = time(NULL);
+    int num = 0;
+    for (int i = 0; i < 1000000000; ++i) {
+//        PRINTLN_INT(isdigit('9'));
+        num = isdigit('9');
+    }
+    time_t t2 = time(NULL);
+    PRINTLN_INT(t2 - t1);
+//    PRINTLN_INT(IsDigit('9'));
     return 0;
 }
