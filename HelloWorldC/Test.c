c8de6a8315c08c9872e7fd400d36eed5ee86244c
mark <978188440@qq.com>
2022-09-16 18:15:55 +0800

    1

--- HelloWorldC/Test.c
+++ HelloWorldC/Test.c
@@ -12,21 +12,21 @@ int main() {
     //?????
     srand(time(NULL));
     int ranndom = rand();
-    puts("?????");
+    puts("输入数字");
     while (true) {
         int user_input;
         scanf("%d", &user_input);
         if (user_input == ranndom) {
-            puts("?????");
+            puts("猜对了");
             break;
         } else if (user_input > ranndom) {
-            puts("????");
+            puts("大了");
         } else {
-            puts("С??");
+            puts("小了");
         }
     }
-    printf("%s\n", "??????????????");
+    printf("%s\n", "恭喜");
     scanf("%s");
-    exit(0);//?????????????????
+    exit(0);//退出
     return 0;
 }
